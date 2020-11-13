2a0864f07b09e8bad1bcc790cff533ca24463cd0
Renat Zubairov <renat.zubairov@gmail.com>
2020-11-13 14:47:25 +0100

    Now exporting to files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { runInContext } from "vm";
 
 const MQTT = require("async-mqtt");
+const util = require('util');
+const fs = require('fs');
 
-const TOPIC = 'km200/status/#';
+const TOPIC = '#';
 const state = {};
+const MAX_FILE_SIZE = '1M';
+const LOG_FILE_NAME = '/tmp/mtqq-exporter/mtqq-log-%DATE%';
+const AUDIT_FILE_NAME = '/tmp/mtqq-exporter/audit-log.json';
+
 
 run();
 
@@ -11,15 +17,6 @@ async function run() {
   console.log("Starting...");
   const client = await MQTT.connectAsync("mqtt://192.168.178.56")
   console.log("Connected to local MQTT");
-  const google = await connectToGoogle();
-  const deviceId = `BuderusHeizung`;
-  setInterval(async () => {
-    console.log(`Sending state to Google Cloud \n ${JSON.stringify(state, null, '\t')}`);
-    const mqttTopic = `/devices/${deviceId}/state`;
-    await google.publish(mqttTopic, JSON.stringify(state));
-    console.log('Published to google');
-  b}, 60000)
-
   try {
     await client.subscribe(TOPIC);
     console.log(`Subscribed to ${TOPIC}`);
@@ -30,104 +27,58 @@ async function run() {
   }
 
   client.on('message', async (t, m) => {
-    console.log(`Topic ${t} has got message ${m.toString()}`);
+    // console.log(`Topic ${t} has got message ${m.toString()}`);
     const path = t.split('/');
     const body = JSON.parse(m.toString());
-    // timestamp = body.ts;
-    delete body.ts;
-    path.shift();
-    path.shift();
-    let root = state;
-    while (path.length > 1) {
-      const next = path.shift();
-      if (!root[next]) {
-        root[next] = {};
+    const type = path.shift();
+    path.shift(); // status
+    if (type == 'hm') {
+      // console.log('Processing HM event');
+      body.source = path.shift();
+      body.event = path.shift();
+      dumpEvent(body);
+    } else if (type == 'km200') {
+      //console.log('Processing KM200 event');
+      let root = state;
+      while (path.length > 1) {
+        const next = path.shift();
+        if (!root[next]) {
+          root[next] = {};
+        }
+        root = root[next]
       }
-      root = root[next]
-    }
-    if (body.km200_unitOfMeasure) {
-      root[path.join('_')] = body;
-    } else {
-      root[path.join('_')] = body.val;
+      if (body.km200_unitOfMeasure) {
+        root[path.join('_')] = body;
+      } else {
+        root[path.join('_')] = body.val;
+      }
     }
   });
 
 }
 
-async function connectToGoogle() {
-  const projectId = `homeiot-295316`;
-  const deviceId = `BuderusHeizung`;
-  const registryId = `Home`;
-  const gatewayId = 'homebridge';
-  const region = `europe-west1`;
-  const mqttBridgeHostname = `mqtt.googleapis.com`;
-  const privateKeyFile = `./resources/rsa_private.pem`
-  const mqttBridgePort = 8883;
-  const messageType = `state`;
-  const algorithm = `RS256`;
-
-
-  // The mqttClientId is a unique string that identifies this device. For Google
-  // Cloud IoT Core, it must be in the format below.
-  const mqttClientId = `projects/${projectId}/locations/${region}/registries/${registryId}/devices/${gatewayId}`;
-
-  // With Google Cloud IoT Core, the username field is ignored, however it must be
-  // non-empty. The password field is used to transmit a JWT to authorize the
-  // device. The "mqtts" protocol causes the library to connect using SSL, which
-  // is required for Cloud IoT Core.
-  const connectionArgs = {
-    host: mqttBridgeHostname,
-    port: mqttBridgePort,
-    clientId: mqttClientId,
-    username: 'unused',
-    password: createJwt(projectId, privateKeyFile, algorithm),
-    protocol: 'mqtts',
-    secureProtocol: 'TLSv1_2_method',
-  };
-  console.log('Connecting to Google MQTT');
-  const client = await MQTT.connectAsync(connectionArgs);
-  console.log('Connected to Google MQTT');
-
-  client.on('error', err => {
-    console.log('error in Google MQTT', err);
-  });
-  client.subscribe(`/devices/${gatewayId}/errors`, { qos: 0 });
-  await attachDevice(deviceId, client);
-  client.on('message', (topic, message) => {
-    const decodedMessage = Buffer.from(message, 'base64').toString('ascii');
-    console.log(`message received on Google error topic ${topic}: ${decodedMessage}`);
-  });
-  client.on('packetsend', () => {
-    console.log('Google Packetsend');
-  });
-  return client;
-}
-
-
-// Create a Cloud IoT Core JWT for the given project id, signed with the given
-// private key.
-// [START iot_mqtt_jwt]
-const createJwt = (projectId, privateKeyFile, algorithm) => {
-  // Create a JWT to authenticate this device. The device will be disconnected
-  // after the token expires, and will have to reconnect with a new token. The
-  // audience field should always be set to the GCP project id.
-  const token = {
-    iat: Math.round(new Date().getTime() / 1000),
-    exp: Math.round(new Date().getTime() / 1000) + 20 * 60, // 20 minutes
-    aud: projectId,
-  };
-  const privateKey = fs.readFileSync(privateKeyFile);
-  return jwt.sign(token, privateKey, { algorithm: algorithm });
-};
-
-// Attaches a device to a gateway.
-const attachDevice = async (deviceId, client, jwt?: undefined) => {
-  const attachTopic = `/devices/${deviceId}/attach`;
-  console.log(`Attaching: ${attachTopic}`);
-  let attachPayload = '{}';
-  if (jwt && jwt !== '') {
-    attachPayload = `{ 'authorization' : ${jwt} }`;
+const logStream = require('file-stream-rotator').getStream(
+  {
+    filename: LOG_FILE_NAME,
+    frequency: "custom",
+    verbose: true,
+    date_format: "YYYY-MM-DD",
+    size: MAX_FILE_SIZE,
+    extension: '.json',
+    max_logs: 10,
+    audit_file: AUDIT_FILE_NAME
   }
-  console.log(`Attaching device ${deviceId} to the gateway`);
-  await client.publish(attachTopic, attachPayload, { qos: 1 });
-};
+);
+logStream.on('rotate', function (oldFile, newFile) {
+  console.log(`File ${oldFile} is ready for upload`);
+  fs.closeSync(fs.openSync(oldFile + '.ready', 'w'));
+});
+
+logStream.on('error', err => console.error(err));
+
+async function dumpEvent(event) {
+  const needDrain = logStream.write(JSON.stringify(event) + '\n', 'utf8');
+  if (needDrain) {
+    await new Promise(cb => logStream.once('drain', cb));
+  }
+}
